cb0c871418022995733eda4ee393d76c82e50d9b
1-vijayan <learnhp.9@gmail.com>
2026-03-10 13:24:11 +0530

    fix duplicate auth routes

--- core/urls.py
+++ core/urls.py
@@ -3,10 +3,10 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 
-    path('', include('habits.urls')),
+    path("", include("habits.urls")),
 
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
+    path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(next_page="/login/"), name="logout"),
 ]
--- habits/urls.py
+++ habits/urls.py
@@ -1,20 +1,15 @@
 from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 
-    path('', views.home, name="home"),
+    path("", views.home, name="home"),
 
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-
-    path('register/', views.register, name="register"),
-
-    path('complete/<int:habit_id>/', views.complete_habit, name="complete"),
-
-    path('delete/<int:habit_id>/', views.delete_habit, name="delete"),
+    path("register/", views.register, name="register"),
 
     path("profile/", views.profile, name="profile"),
+
+    path("complete/<int:habit_id>/", views.complete_habit, name="complete"),
+
+    path("delete/<int:habit_id>/", views.delete_habit, name="delete"),
 ]
